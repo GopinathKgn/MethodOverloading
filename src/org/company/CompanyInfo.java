19f103f353efd25e726be1643c69653f8b1d3e64
GOpinathKgn <gopikaruppasamy@gmail.com>
2023-04-25 19:19:44 +0530

    CompanyDeatils v 1.0

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName(String name) {
+		System.out.println("Company Name :"+name);
+
+	}
+	private void companyName(int id, long c) {
+		System.out.println("Company ID:"+id+"\nContact Number :"+c);
+		
+}
+	private void companyName(long l, String A) {
+	System.out.println("Company License No :"+l+"\nCompany Address :"+A);
+	
+	}
+public static void main(String[] args) {
+	CompanyInfo c = new CompanyInfo();
+	c.companyName("HCL");
+	c.companyName(1122, 8778803877l);
+	c.companyName(5347824l, "No 602/3, Elcot Economic Zone,\n Medavakkam High Road, \n Sholinganallur, Chennai - 600119.");
+}
+
+}
+
